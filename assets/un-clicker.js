f8bd77862fe4485818cfb61e6c7c4bccd29d109f
Flo <flo@MacBook-de-Anne.local>
2019-11-12 17:16:29 +0100

    style et décompteur fonctionnel

--- assets/un-clicker.js
+++ assets/un-clicker.js
@@ -1,12 +1,21 @@
 var evilValue = 10e5
+var currentValue = evilValue
+var affichage = document.getElementById("dollar")
+var power = 0
+var secondes = new Date.getSeconds()
 
-document.write(evilValue)
 
 function init() {
   var bouton = document.getElementById("attac")
   bouton.onclick = attac
 }
 
-function attac(event) {
-  alert("coucou les gens")
+function attac(event, power) {
+  power = 5
+  affichage.removeChild(affichage.childNodes[0])
+  var newdiv=document.createElement('div')
+  currentValue = currentValue - power
+  newdiv.textContent = currentValue
+  affichage.appendChild(newdiv)
 }
+
