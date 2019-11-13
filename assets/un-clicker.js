d0af2545ef21240aa2ad6171b88f96063d7fe3c3
Flo <flo@MacBook-de-Anne.local>
2019-11-13 11:46:03 +0100

    yolo

--- assets/un-clicker.js
+++ assets/un-clicker.js
@@ -2,8 +2,7 @@ var evilValue = 10e5
 var currentValue = evilValue
 var affichage = document.getElementById("dollar")
 var power = 0
-var secondes = new Date.getSeconds()
-
+var start = Date.now()
 
 function init() {
   var bouton = document.getElementById("attac")
@@ -11,11 +10,12 @@ function init() {
 }
 
 function attac(event, power) {
+  var time = Date.now()
+  var seconds = parseInt((time - start)/1000)
   power = 5
   affichage.removeChild(affichage.childNodes[0])
   var newdiv=document.createElement('div')
   currentValue = currentValue - power
-  newdiv.textContent = currentValue
-  affichage.appendChild(newdiv)
+  newdiv.textContent = currentValue+"\n"+seconds
+  affichage.appendChild(newdiv) 
 }
-
