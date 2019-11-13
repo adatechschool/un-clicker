d6a096853a52501b81958f20e80262d38941220f
Flo <flo@MacBook-de-Anne.local>
2019-11-13 14:55:51 +0100

    des trucs et d'autres trucs

--- scripts/un-clicker.js
+++ scripts/un-clicker.js
@@ -1,6 +1,7 @@
 var evilValue = 10e5
 var currentValue = evilValue
 var affichage = document.getElementById("dollar")
+var showhope = document.getElementById("hope")
 var power = 0
 var start = Date.now()
 var step = start
@@ -8,16 +9,18 @@ var step = start
 function init() 
 {
   var bouton = document.getElementById("attac")
+  var bouton2 = document.getElementById("attac")
+  bouton2.onclick = espoir
   bouton.onclick = attac
 }
 
-var espoir = 0
+var hope = 0
+
 function attac(event, power) 
 {
   var time = Date.now()
   var seconds = parseInt((time-step)/1000)
   affichage.removeChild(affichage.childNodes[0])
-  espoir++
   power = 5
   var newdiv=document.createElement('div')
   currentValue = currentValue + seconds
@@ -27,14 +30,27 @@ function attac(event, power)
   step = time
 }
 
-function espoir(event) 
-{
-    var showhope = document.getElementById("hope")
-    var newhope = document.createElement('div')
-    showhope.removeChild(showhope.childNodes[0])
-    newhope.textContent = espoir
-    showhope.appendChild(newhope)
+function stackmoney() 
+{ 
+  affichage.removeChild(affichage.childNodes[0])
+  var newdiv=document.createElement('div')
+  currentValue++
+  newdiv.textContent = currentValue
+  affichage.appendChild(newdiv)
 }
 
+function refresh()
+{
+    var refresh = setInterval(stackmoney(), 1000)
+}
 
+refresh();
 
+function espoir(event) 
+{
+    hope++
+    var newhope = document.createElement('div')
+    showhope.removeChild(showhope.childNodes[0])
+    newhope.textContent = hope
+    showhope.appendChild(newhope)
+}
