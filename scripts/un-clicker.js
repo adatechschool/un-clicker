d4284308ebe4a58cce2abc3f22e219937e065f5e
Flo <flo@MacBook-de-Anne.local>
2019-11-13 14:00:29 +0100

    monopoly

--- scripts/un-clicker.js
+++ scripts/un-clicker.js
@@ -3,19 +3,38 @@ var currentValue = evilValue
 var affichage = document.getElementById("dollar")
 var power = 0
 var start = Date.now()
+var step = start
 
-function init() {
+function init() 
+{
   var bouton = document.getElementById("attac")
   bouton.onclick = attac
 }
 
-function attac(event, power) {
+var espoir = 0
+function attac(event, power) 
+{
   var time = Date.now()
-  var seconds = parseInt((time - start)/1000)
-  power = 5
+  var seconds = parseInt((time-step)/1000)
   affichage.removeChild(affichage.childNodes[0])
+  espoir++
+  power = 5
   var newdiv=document.createElement('div')
+  currentValue = currentValue + seconds
   currentValue = currentValue - power
-  newdiv.textContent = currentValue+"\n"+seconds
-  affichage.appendChild(newdiv) 
+  newdiv.textContent = currentValue
+  affichage.appendChild(newdiv)
+  step = time
 }
+
+function espoir(event) 
+{
+    var showhope = document.getElementById("hope")
+    var newhope = document.createElement('div')
+    showhope.removeChild(showhope.childNodes[0])
+    newhope.textContent = espoir
+    showhope.appendChild(newhope)
+}
+
+
+
