28ee67dd23d71ece33a1e3e16d087f33fd7290b7
Leirda01 <adrieldj@orange.fr>
2019-11-14 15:28:53 +0100

    Créer une liste d’améliorations à débloquer

--- scripts/un-clicker.js
+++ scripts/un-clicker.js
@@ -1,28 +1,43 @@
-var currentValue = 10e5
-var showDollars = document.getElementById("dollar")
-var showHope = document.getElementById("hope")
-var hope = 0
-showHope.innerHTML = hope
-showDollars.innerHTML = currentValue
+var bouton = document.getElementById("attac");
+var showDollars = document.getElementById("dollar");
+var showHope = document.getElementById("hope");
 
-var bouton = document.getElementById("attac")
-bouton.addEventListener("click", attac, false)
-bouton.addEventListener("click", espoir, false)
+var upgrade = [
+  {
+    name: "Enceinte",
+    description: "Une enceinte pour emmerder les vieilles dans le métro.",
+    niveau: 50
+  },
+  {
+    name: "Lycéen",
+    description: "Un jeune mal dans sa peau aisément manipulable.",
+    niveau: 70
+  },
+];
+
+var currentValue = 10e5;
+var hope = 0;
+
+showHope.innerText = hope;
+showDollars.innerText = currentValue;
+bouton.addEventListener("click", attac, false);
+bouton.addEventListener("click", espoir, false);
+
 
 function attac(event) 
 {
-  currentValue = currentValue - 5
-  showDollars.innerHTML = currentValue
+  currentValue = currentValue - 5;
+  showDollars.innerHTML = currentValue;
 }
 
-setInterval("stackmoney()", 300)
+setInterval("stackmoney()", 300);
 
 function stackmoney() 
 { 
-  showDollars.innerText = currentValue++
+  showDollars.innerText = currentValue++;
 }
 
-function espoir(event) 
+function espoir() 
 {
-  showHope.innerText = hope++
+  showHope.innerText = hope++;
 }
